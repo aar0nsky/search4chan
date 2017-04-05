dc36163aebeec6df34d17fe6be2097207f3df4d7
Unknown <a.ankiel@gmail.com>
2017-04-04 20:42:06 -0400

    Added full functionality. Opening link broken.

--- src/main/java/me/aar0nsky/search4Chan/App.java
+++ src/main/java/me/aar0nsky/search4Chan/App.java
@@ -2,6 +2,7 @@ package me.aar0nsky.search4Chan;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.io.IOException;
 
 import org.apache.http.HttpEntity;
@@ -20,24 +21,44 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-    	//String myURL = "http://httpbin.org/get";
-    	String myURL = "http://httpbin.org";
-    	String searchTerm = "delete";
-    	try {
-    		getHTTP(myURL, searchTerm);
-    	}
-    	catch (IOException e) {
+    	//String MYURL = "http://httpbin.org/get";
+    	//public static final String MYURL = "http://httpbin.org";
+    	//public static final String SEARCHTERM = "delete";
+    	String MYURL = "http://boards.4chan.org/";
+    	String SEARCHTERM = "delete";
+    	String BOARD = "b";
+    	Scanner scanzor = new Scanner(System.in);
+
+    	System.out.println("Searching: " + MYURL);
+    	System.out.println("Enter board: ");
+    	BOARD = scanzor.nextLine();
+    	System.out.println("Enter search term: ");
+    	SEARCHTERM = scanzor.nextLine();
+
+    	MYURL = MYURL + BOARD + "/";
+
+    	for(int i = 0;i < 10;i++) {
+    		
+    		try {
+    		searchPage(MYURL + i, SEARCHTERM);
+    		}
+    		catch (IOException e) {
     		System.out.println("IOException occured");
+    		System.out.println("Search Term: " + SEARCHTERM);
+    		System.out.println("URL: " + MYURL);
     		e.printStackTrace(System.out);
+    		}
+
     	}
+    	
     }
 
-    private static void getHTTP(String getURL, String searchTerm) throws IOException {
+    private static void searchPage(String myURL, String searchTerm) throws IOException {
 
     	CloseableHttpClient httpclient = HttpClients.createDefault();
 
         try {
-            HttpGet httpGet = new HttpGet(getURL);
+            HttpGet httpGet = new HttpGet(myURL);
             CloseableHttpResponse response1 = httpclient.execute(httpGet);
 
             try {
@@ -47,15 +68,16 @@ public class App
                 if (status.indexOf("200") > 0) {
                 	//System.out.println("HTTP Success!");
                		HttpEntity entity1 = response1.getEntity();
+               		// put page contents into a string
                		String pageBody = EntityUtils.toString(entity1);
               // 	System.out.println(pageBody);
-
+               	// check if search term is on page
                	if(pageBody.indexOf(searchTerm) > 0) {
-               		System.out.println("This page contains: " + searchTerm);
-
+               		//System.out.println("This page contains: " + searchTerm);
+               		// open url 
+               		Runtime.getRuntime().exec(myURL);
                	}
-                // do something useful with the response body
-                // and ensure it is fully consumed
+                // fully consume
                 EntityUtils.consume(entity1);
                 }
 
