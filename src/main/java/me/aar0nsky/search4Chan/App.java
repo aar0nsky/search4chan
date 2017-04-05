ecc4bbd720c8055117278f4449a7e6484bfa32db
Unknown <a.ankiel@gmail.com>
2017-04-04 21:22:46 -0400

    Added super resilient code for opening a URL cross platform thanks to mkyong.com.

--- src/main/java/me/aar0nsky/search4Chan/App.java
+++ src/main/java/me/aar0nsky/search4Chan/App.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.Desktop;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -24,6 +27,7 @@ public class App
     	//String MYURL = "http://httpbin.org/get";
     	//public static final String MYURL = "http://httpbin.org";
     	//public static final String SEARCHTERM = "delete";
+
     	String MYURL = "http://boards.4chan.org/";
     	String SEARCHTERM = "delete";
     	String BOARD = "b";
@@ -75,7 +79,18 @@ public class App
                	if(pageBody.indexOf(searchTerm) > 0) {
                		//System.out.println("This page contains: " + searchTerm);
                		// open url 
-               		Runtime.getRuntime().exec(myURL);
+               		openBrowsermkyong(myURL);
+
+               		// only works on windows?
+               		// Desktop desktop = java.awt.Desktop.getDesktop();
+               		// try {
+               		// desktop.browse(convertURL(myURL));
+               		// }
+               		// catch (URISyntaxException e) {
+
+               		// }
+               		// ?? this method didnt work either
+               		//Runtime.getRuntime().exec(myURL);
                	}
                 // fully consume
                 EntityUtils.consume(entity1);
@@ -90,4 +105,48 @@ public class App
         }	
     }
 
+    // public static URI convertURL(String URL) throws URISyntaxException {
+    //            		URI myURI = new URI(URL);
+    //            		return myURI;
+    // }
+
+    // taken from mkyong.com
+    // after being frustrated with linux not opening URLs like windows
+    public static void openBrowsermkyong (String url) {
+    		
+    		String os = System.getProperty("os.name").toLowerCase();
+			Runtime rt = Runtime.getRuntime();
+
+    		try{
+
+	    if (os.indexOf( "win" ) >= 0) {
+
+	        // this doesn't support showing urls in the form of "page.html#nameLink"
+	        rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+
+	    } else if (os.indexOf( "mac" ) >= 0) {
+
+	        rt.exec( "open " + url);
+
+            } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+
+	        // Do a best guess on unix until we get a platform independent way
+	        // Build a list of browsers to try, in this order.
+	        String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+	       			             "netscape","opera","links","lynx"};
+
+	        // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+	        StringBuffer cmd = new StringBuffer();
+	        for (int i=0; i<browsers.length; i++)
+	            cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
+
+	        rt.exec(new String[] { "sh", "-c", cmd.toString() });
+
+           } else {
+                return;
+           }
+       }catch (Exception e){
+	    return;
+       }
+    }
 }
